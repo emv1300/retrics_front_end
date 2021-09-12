d74ee1b1dbffc7fa89ca05bc13eed2243f2b2a05
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-09-12 16:48:42 +0100

    added incomplete sidebar and diamond card

--- src/components/mainPage.js
+++ src/components/mainPage.js
@@ -2,34 +2,47 @@ import React from 'react';
 import whiteLogo from '../assets/tradeify_logo_white.png'
 import {BiUserCircle} from 'react-icons/bi'
 import '../css/dashboard.css';
+import DiamondCard from './cards/diamondCard';
 
 function DashBoard({Logout}){
 
-    
+    const accountBalance = localStorage.getItem('user_balance');
+
+
     const NextDay = () => {
         console.log("next day")
     }
-
+    
     return(
-        <React.Fragment>
-            
-            <div className = "topNavBar">
-            
-                
-                <img className = "whiteLogo" alt = "website logo" src = {whiteLogo}  heigth = "120" width = "240" Align = 'center' />
-                
-                <p className = "balance"> balance here </p>
-                <button className = "nextDayBT" onClick = {NextDay}> Next day </button>
-                <button className = "logoutBT" onClick = {Logout}>
-                <BiUserCircle className = "userIcon" size = "2em"/>      
-                </button>
-            
+        <div>
+            <div className="topNavBar">
+                <img
+                className="whiteLogo"
+                alt="website logo"
+                src={whiteLogo}
+                heigth="120"
+                width="240"
+                />
 
+                <p className="balance"> {accountBalance} $</p>
+                <button className="nextDayBT" onClick={NextDay}>
+                {" "}
+                Next day{" "}
+                </button>
+                <button className="logoutBT" onClick={Logout}>
+                <BiUserCircle className="userIcon" size="2em" />
+                </button>
             </div>
-            
-            
-        </React.Fragment>
-        
+            <div className="lineUpElements">
+                <div className="sideBar"></div>
+                <div className="gemCards">
+                    <div className="dSpace">
+                        <DiamondCard/>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>       
     )
 }
 
