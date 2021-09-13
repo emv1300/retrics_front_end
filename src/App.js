5b385e63356e0d7dee8696a6058c5150888fe9a3
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-09-13 18:42:36 +0100

    switched context providers

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { useState, useContext  } from 'react';
 import LoginForm from './components/login';
 import DashBoard from './components/mainPage';
 import { UserContext } from './components/authContext';
+import AuthenticationContext,{ AuthenticationContextProvider } from './components/contextUser';
 
 function App() {
 
-  const {currentLoggedInUser} = useContext(UserContext);
+  const {currentLoggedInUser} = useContext(AuthenticationContext);
 
   const LogoutUser = () => {
     console.log("logged out");
   }
 
   return (
-    <UserContext.Provider value = {{userC, setUserC}}>
+    <AuthenticationContextProvider>
       <div className="App">
         {
           (currentLoggedInUser !== "") ? (
@@ -27,7 +28,7 @@ function App() {
           )
         }
       </div>
-    </UserContext.Provider>
+    </AuthenticationContextProvider>
   );
 }
 
