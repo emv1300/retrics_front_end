48e3dd0d2c170c9d45293b60fc66ec1568a78035
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-09-13 18:37:35 +0100

    moved login function from app.js to login.js

--- src/components/login.js
+++ src/components/login.js
@@ -2,9 +2,80 @@ import React, { useState } from 'react';
 import tradeify_logo from '../assets/tradeify_logo.png';
 import '../css/app.css';
 
-function LoginForm({ Login, error}) {
+function LoginForm() {
+
     const [details, setDetails] = useState({email:"", password:""});
+    const [user, setUser] = useState({email: "", password: ""});
+    const [error, setError] = useState("");
     
+    const [userC, setUserC] = useState("");
+
+    let users = [];
+  
+    fetch("http://localhost:3000/users")
+    .then(response => response.json()).then(data => {
+        data.forEach(element => {
+            let x = {
+                email: element.email,
+                password: element.password,
+                accountBalance: element.accountBalance,
+                diamondsOwned: element.diamond,
+                emeraldsOwned: element.emerald,
+                rubiesOwned: element.ruby
+            }
+            users.push(x);
+        });
+    }).then(console.log(users));
+
+    let gems = [];
+    
+    fetch("http://localhost:3000/jewels")
+    .then(response => response.json()).then(data => {
+        data.forEach(element => {
+            let x = {
+                id: element.id,
+                name: element.name,
+                value: element.value
+            }
+            gems.push(x);
+        });
+    }).then(console.log(gems))
+    
+    const Login = async (details)  => {
+ 
+        try{
+          let currentUser = users.find(element => element.email.toLowerCase() === details.email.toLowerCase());
+          let indGem = gems.find(element => element.name === 'diamond');
+          localStorage.setItem('diamond', indGem.value);
+          indGem = gems.find(element => element.name === 'emerald');
+          localStorage.setItem('emerald', indGem.value);
+          indGem = gems.find(element => element.name === 'ruby');
+          localStorage.setItem('ruby', indGem.value);
+          console.log(currentUser)
+          if(currentUser.password === details.password) {
+          
+            setUser({
+              email: details.email
+            });
+            setUserC(currentUser.email)
+            
+            localStorage.setItem('user_email', details.email);
+            localStorage.setItem('user_balance', currentUser.accountBalance);
+            localStorage.setItem('diamondsOwned', currentUser.diamondsOwned);
+            localStorage.setItem('emeraldsOwned', currentUser.emeraldsOwned);
+            localStorage.setItem('rubiesOwned', currentUser.rubiesOwned);
+    
+            //await currentLoggedInUser();
+          }else{
+            console.log("details don't match!");
+            setError("details don't match!");
+          }
+        }catch(e){
+          console.log("details don't match!");
+          setError("details don't match!");
+        }  
+      }
+
     const submitHandler = e => {
         e.preventDefault();
         
