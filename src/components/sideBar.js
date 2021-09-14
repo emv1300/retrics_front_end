fd65f56c09f747080e43e5d50a3c7ce40333daf9
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-09-14 11:35:11 +0100

    created sidebar component
    users can now navigate between statistics and trade routes

---
+++ src/components/sideBar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { SideBarData } from "./sideBarData";
+import "../css/sidebar.css"
+
+function SideBar() {
+
+    return(
+        <div className = "SideBar">
+            <ul className = "SideBarList">
+            {
+                SideBarData.map((val, key) =>{
+
+                    return(
+
+                        <li className = "SideBarListElements" key = {key} onClick = {() => {window.location.pathname= val.link}}>
+                            <div> {val.title} </div>
+                        </li>
+
+                    )
+
+                })
+
+            }
+            </ul>
+        </div>
+    )
+}
+
+export default SideBar;
