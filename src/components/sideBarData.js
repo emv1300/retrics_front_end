e33a23bf1e4c926351cc40ade360230c3b9ddcc4
enem2@coventry.ac.uk <enem2@coventry.ac.uk>
2021-09-14 11:29:49 +0100

    added new file that contains side bar data

---
+++ src/components/sideBarData.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const SideBarData = [
+    {
+        title : "trade",
+        link: "/trade"
+    },
+    {   
+        title: "statistis",
+        link: "statistics"
+    }
+]
